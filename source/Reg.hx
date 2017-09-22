a1b7ce200fbab7013e86abe8daae4996a0f543d3
Unknown <nacho7577@hotmail.com>
2017-09-22 17:24:18 -0300

    Second Push

---
+++ source/Reg.hx
@@ -0,0 +1,15 @@
+package;
+
+/**
+ * ...
+ * @author 
+ */
+class Reg 
+{
+
+	public function new() 
+	{
+		
+	}
+	
+}
--- source/entities/Player.hx
+++ source/entities/Player.hx
@@ -1,15 +1,34 @@
 package entities;
 
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.system.FlxAssets.FlxGraphicAsset;
 
 class Player extends FlxSprite 
 {
-
+	private var speed:Int;
+	
 	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
 	{
 		super(X, Y, SimpleGraphic);
 		
+		speed = Reg.playerNormalSpeed;
+	}
+	
+	override public function update(elapsed:Float):Void
+	{
+		super.update(elapsed);
+		
+		velocty.set(Reg.cameraSpeed, 0);
+		
+		if (FlxG.keys.pressed.UP)
+			velocity.y -= speed;
+		if (FlxG.keys.pressed.DOWN)
+			velocity.y += speed;
+		if (FlxG.keys.pressed.LEFT)
+			velocity.x -= speed);
+		if (FlxG.keys.pressed.RIGHT)
+			velocity.
 	}
 	
 }
